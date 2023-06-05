7f204da37e45880abbd3e6ade296664d08c440ae
Abdallah Moubarak <abdallah.moubarak02@gmail.com>
2023-06-06 01:23:12 +0300

    setting nextauth

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,32 +1,54 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[next-auth]/route";
 
 export async function GET() {
-  const products = await prisma.product.findMany();
-  return NextResponse.json({ products });
+  const products = await prisma.product.findMany({});
+  return NextResponse.json({ products }, { status: 200 });
 }
 
 export async function POST(request: Request) {
-  const { name, price } = await request.json();
-  const product = await prisma.product.create({
-    data: {
-      name,
-      price,
-    },
-  });
+  const session = await getServerSession(authOptions);
+  if (!session)
+    return NextResponse.json(
+      { message: "You are not logged in." },
+      { status: 401 },
+    );
+  try {
+    const { name, price } = await request.json();
+    const product = await prisma.product.create({
+      data: {
+        name,
+        price,
+      },
+    });
 
-  return NextResponse.json({ product });
+    return NextResponse.json({ product }, { status: 200 });
+  } catch (err) {
+    return NextResponse.error;
+  }
 }
 
 export async function PUT(request: Request) {
-  const { id, name, price } = await request.json();
-  const product = await prisma.product.update({
-    where: { id },
-    data: {
-      name,
-      price,
-    },
-  });
+  const session = await getServerSession(authOptions);
+  if (!session)
+    return NextResponse.json(
+      { message: "You are not logged in." },
+      { status: 401 },
+    );
+  try {
+    const { id, name, price } = await request.json();
+    const product = await prisma.product.update({
+      where: { id },
+      data: {
+        name,
+        price,
+      },
+    });
 
-  return NextResponse.json({ product });
+    return NextResponse.json({ product });
+  } catch (err) {
+    return NextResponse.error;
+  }
 }
